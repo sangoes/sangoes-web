926017103a7b023a48ecbcca0ff8bca3dd77b516
jerrychir <jerrychir@163.com>
2018-10-30 14:05:24 +0800

    add config and less

--- .umirc.js
+++ .umirc.js
@@ -1,18 +1,30 @@
-
+import path from 'path';
 // ref: https://umijs.org/config/
 export default {
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      dynamicImport: true,
-      title: 'sangoes-web',
-      dll: true,
-      routes: {
-        exclude: [],
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: true,
+        dynamicImport: true,
+        title: 'sangoes-web',
+        dll: true,
+        routes: {
+          exclude: [],
+        },
+        dll: {
+          exclude: [],
+          include: ['dva', 'dva/router', 'dva/saga', 'dva/fetch', 'antd/es'],
+        },
+        hardSource: true,
       },
-      hardSource: true,
-    }],
+    ],
   ],
-}
+  targets: {
+    ie: 10,
+  },
+  history: 'hash',
+  extraBabelPlugins: [['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
+};
