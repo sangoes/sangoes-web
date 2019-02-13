22d5dd199a90b0b4a322e6d1b21b0e251a49e25a
Jerry Chir <jerrychir@gmail.com>
2019-02-13 18:45:58 +0800

    changge globalHeader

--- src/components/GlobalHeader/RightContent.js
+++ src/components/GlobalHeader/RightContent.js
@@ -1,11 +1,10 @@
 import React, { PureComponent } from 'react';
 import { FormattedMessage, formatMessage } from 'umi/locale';
-import { Spin, Tag, Menu, Icon, Dropdown, Avatar, Tooltip } from 'antd';
+import { Spin, Tag, Menu, Icon, Avatar, Tooltip } from 'antd';
 import moment from 'moment';
 import groupBy from 'lodash/groupBy';
-// import SelectLang from '../SelectLang';
 import styles from './index.less';
-import { NoticeIcon, HeaderSearch } from 'ant-design-pro';
+import { NoticeIcon, HeaderSearch, HeaderDropdown } from 'ant-design-pro';
 
 export default class GlobalHeaderRight extends PureComponent {
   getNoticeData() {
@@ -39,13 +38,53 @@ export default class GlobalHeaderRight extends PureComponent {
     return groupBy(newNotices, 'type');
   }
 
+  getUnreadData = noticeData => {
+    const unreadMsg = {};
+    Object.entries(noticeData).forEach(([key, value]) => {
+      if (!unreadMsg[key]) {
+        unreadMsg[key] = 0;
+      }
+      if (Array.isArray(value)) {
+        unreadMsg[key] = value.filter(item => !item.read).length;
+      }
+    });
+    return unreadMsg;
+  };
+
+  changeReadState = clickedItem => {
+    const { id } = clickedItem;
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'global/changeNoticeReadState',
+      payload: id,
+    });
+  };
+
+  fetchMoreNotices = tabProps => {
+    const { list, name } = tabProps;
+    const { dispatch, notices = [] } = this.props;
+    const lastItemId = notices[notices.length - 1].id;
+    dispatch({
+      type: 'global/fetchMoreNotices',
+      payload: {
+        lastItemId,
+        type: name,
+        offset: list.length,
+      },
+    });
+  };
+
   render() {
     const {
       currentUser,
+      fetchingMoreNotices,
       fetchingNotices,
+      loadedAllNotices,
       onNoticeVisibleChange,
       onMenuClick,
       onNoticeClear,
+      skeletonCount,
+      onNoticeTabChange,
       theme,
     } = this.props;
     const menu = (
@@ -54,9 +93,13 @@ export default class GlobalHeaderRight extends PureComponent {
           <Icon type="user" />
           <FormattedMessage id="menu.account.center" defaultMessage="account center" />
         </Menu.Item>
-        <Menu.Item key="userSetting">
+        <Menu.Item key="userinfo">
           <Icon type="setting" />
           <FormattedMessage id="menu.account.settings" defaultMessage="account settings" />
+        </Menu.Item>
+        <Menu.Item key="triggerError">
+          <Icon type="close-circle" />
+          <FormattedMessage id="menu.account.trigger" defaultMessage="Trigger Error" />
         </Menu.Item>
         <Menu.Divider />
         <Menu.Item key="logout">
@@ -65,7 +108,13 @@ export default class GlobalHeaderRight extends PureComponent {
         </Menu.Item>
       </Menu>
     );
+    const loadMoreProps = {
+      skeletonCount,
+      loadedAll: loadedAllNotices,
+      loading: fetchingMoreNotices,
+    };
     const noticeData = this.getNoticeData();
+    const unreadMsg = this.getUnreadData(noticeData);
     let className = styles.right;
     if (theme === 'dark') {
       className = `${styles.right}  ${styles.dark}`;
@@ -76,15 +125,9 @@ export default class GlobalHeaderRight extends PureComponent {
           className={`${styles.action} ${styles.search}`}
           placeholder={formatMessage({ id: 'component.globalHeader.search' })}
           dataSource={[
-            formatMessage({
-              id: 'component.globalHeader.search.example1',
-            }),
-            formatMessage({
-              id: 'component.globalHeader.search.example2',
-            }),
-            formatMessage({
-              id: 'component.globalHeader.search.example3',
-            }),
+            formatMessage({ id: 'component.globalHeader.search.example1' }),
+            formatMessage({ id: 'component.globalHeader.search.example2' }),
+            formatMessage({ id: 'component.globalHeader.search.example3' }),
           ]}
           onSearch={value => {
             console.log('input', value); // eslint-disable-line
@@ -96,7 +139,7 @@ export default class GlobalHeaderRight extends PureComponent {
         <Tooltip title={formatMessage({ id: 'component.globalHeader.help' })}>
           <a
             target="_blank"
-            href="http://sangoes.com"
+            href="https://pro.ant.design/docs/getting-started"
             rel="noopener noreferrer"
             className={styles.action}
           >
@@ -105,59 +148,54 @@ export default class GlobalHeaderRight extends PureComponent {
         </Tooltip>
         <NoticeIcon
           className={styles.action}
-          count={currentUser.notifyCount}
+          count={currentUser.unreadCount}
           onItemClick={(item, tabProps) => {
             console.log(item, tabProps); // eslint-disable-line
+            this.changeReadState(item, tabProps);
           }}
           locale={{
-            emptyText: formatMessage({
-              id: 'component.noticeIcon.empty',
-            }),
-            clear: formatMessage({
-              id: 'component.noticeIcon.clear',
-            }),
+            emptyText: formatMessage({ id: 'component.noticeIcon.empty' }),
+            clear: formatMessage({ id: 'component.noticeIcon.clear' }),
+            loadedAll: formatMessage({ id: 'component.noticeIcon.loaded' }),
+            loadMore: formatMessage({ id: 'component.noticeIcon.loading-more' }),
           }}
           onClear={onNoticeClear}
+          onLoadMore={this.fetchMoreNotices}
           onPopupVisibleChange={onNoticeVisibleChange}
           loading={fetchingNotices}
-          popupAlign={{ offset: [20, -16] }}
+          clearClose
+          onTabChange={onNoticeTabChange}
         >
           <NoticeIcon.Tab
+            count={unreadMsg.notification}
             list={noticeData.notification}
-            title={formatMessage({
-              id: 'component.globalHeader.notification',
-            })}
+            title={formatMessage({ id: 'component.globalHeader.notification' })}
             name="notification"
-            emptyText={formatMessage({
-              id: 'component.globalHeader.notification.empty',
-            })}
+            emptyText={formatMessage({ id: 'component.globalHeader.notification.empty' })}
             emptyImage="https://gw.alipayobjects.com/zos/rmsportal/wAhyIChODzsoKIOBHcBk.svg"
+            {...loadMoreProps}
           />
           <NoticeIcon.Tab
+            count={unreadMsg.message}
             list={noticeData.message}
-            title={formatMessage({
-              id: 'component.globalHeader.message',
-            })}
+            title={formatMessage({ id: 'component.globalHeader.message' })}
             name="message"
-            emptyText={formatMessage({
-              id: 'component.globalHeader.message.empty',
-            })}
+            emptyText={formatMessage({ id: 'component.globalHeader.message.empty' })}
             emptyImage="https://gw.alipayobjects.com/zos/rmsportal/sAuJeJzSKbUmHfBQRzmZ.svg"
+            {...loadMoreProps}
           />
           <NoticeIcon.Tab
+            count={unreadMsg.event}
             list={noticeData.event}
-            title={formatMessage({
-              id: 'component.globalHeader.event',
-            })}
+            title={formatMessage({ id: 'component.globalHeader.event' })}
             name="event"
-            emptyText={formatMessage({
-              id: 'component.globalHeader.event.empty',
-            })}
+            emptyText={formatMessage({ id: 'component.globalHeader.event.empty' })}
             emptyImage="https://gw.alipayobjects.com/zos/rmsportal/HsIsxMZiWKrNUavQUXqx.svg"
+            {...loadMoreProps}
           />
         </NoticeIcon>
         {currentUser.realName ? (
-          <Dropdown overlay={menu}>
+          <HeaderDropdown overlay={menu}>
             <span className={`${styles.action} ${styles.account}`}>
               <Avatar
                 size="small"
@@ -167,11 +205,10 @@ export default class GlobalHeaderRight extends PureComponent {
               />
               <span className={styles.name}>{currentUser.realName}</span>
             </span>
-          </Dropdown>
+          </HeaderDropdown>
         ) : (
           <Spin size="small" style={{ marginLeft: 8, marginRight: 8 }} />
         )}
-        {/* 语言选择 */}
         {/* <SelectLang className={styles.action} /> */}
       </div>
     );
